42a58d79e0063a556f4ebc7fa8cf8f6a123c3622
James Carnegie <james@atomist.com>
2018-04-09 22:19:35 +0100

    Stubs

---
+++ project.clj
@@ -0,0 +1,16 @@
+
+(defproject lein-atomist "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Apache-2.0"}
+  :vcs :git
+  :dependencies [[com.atomist/automation-client-clj "0.3.15"]
+
+                 ;; logging
+                 [io.clj/logging "0.8.1"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [org.slf4j/jcl-over-slf4j "1.7.25"]
+                 [org.slf4j/jul-to-slf4j "1.7.25"]
+                 [org.slf4j/log4j-over-slf4j "1.7.25"]
+                 [org.slf4j/slf4j-api "1.7.25"]]
+  :eval-in-leiningen true)
---
+++ src/leiningen/atomist.clj
@@ -0,0 +1,35 @@
+(ns leiningen.atomist
+  (:require [com.atomist.automation.registry]
+            [com.atomist.automation.core]
+            [com.atomist.automation.config-service]
+            [mount.core :as mount]
+            [clojure.tools.logging :as log]))
+
+(defn ^{:event {:name "pushes-to-repo"
+                :secrets ["github://org_token?scopes=repo"]
+                :description "Trigger builds based on pushes"
+                :subscription "subscription pushes {
+                                  Push {
+                                    repo {
+                                      name
+                                    }
+                                  }
+                                }"}}
+pushes-to-repo
+  [event]
+  (println "A push!"))
+
+(defn atomist
+  "Build this project with Atomist"
+  [project & args]
+  (println "Connecting to Atomist...")
+  (println (mount/start))
+  #_(println
+     (-> (mount/only #{#'com.atomist.automation.config-service/env
+                       #'com.atomist.automation.registry/registrations
+                       #'com.atomist.automation.core/api-connection})
+         (mount/with-args {:config ["resources/config.edn"]})
+         (mount/start)))
+  (log/error "Connected...")
+  (.join (Thread/currentThread)))
+
